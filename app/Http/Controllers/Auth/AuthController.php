52e1e0e692154c4ca67dc7669721b3baf5aef1d6
Yan Gonçalves <yanw100@gmail.com>
2022-12-27 22:59:19 -0300

    [Feature] Add response code.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function signup(SignupRequest $request): JsonResponse
     {
         $user = $this->authService->register($request->validated());
-        return response()->json(new LoginResource($user));
+        return response()->json(new LoginResource($user), 201);
     }
 
     /**
